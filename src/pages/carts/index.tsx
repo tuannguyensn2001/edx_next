0a2676be66e89485764f926527ee69eb5833e63a
Tuan Nguyen Van - 19000372 <devpro2001@gmail.com>
2022-04-16 15:13:23 +0700

    updatE

--- src/pages/carts/index.tsx
+++ src/pages/carts/index.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import DefaultLayout from '@/layouts/Default/index';
-import { CartLayout } from '@/components/carts';
+// import DefaultLayout from '@/layouts/Default/index';
+// import { CartLayout } from '@/components/carts';
+import DefaultLayout from 'layouts/Default';
 
 const CartsPage = () => {
-    return (
-        <DefaultLayout>
-            <CartLayout />
-        </DefaultLayout>
-    );
+    return <DefaultLayout>{/*<CartLayout />*/}</DefaultLayout>;
 };
 
 export default CartsPage;
